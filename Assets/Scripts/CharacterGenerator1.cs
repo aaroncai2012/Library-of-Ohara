0f448b8f91b1bd82f02b0d8e5a02d4375129315a
cc chou <863932688@qq.com>
2023-04-06 21:38:30 -0400

    simple character generator v0.1

--- Assets/Scripts/CharacterGenerator1.cs
+++ Assets/Scripts/CharacterGenerator1.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,43 +9,57 @@ public class CharacterGenerator1 : MonoBehaviour
     public Canvas canvas;
     public Image charPrefab;
 
-    public string word;
+    [TextAreaAttribute] public string input;
     public float offsetX;
     public float offsetY;
+    public float wordSpacing;
+    public float lineSpacing;
 
+    public int wordNumPerLine;
     [HideInInspector] public Sprite[] characters; // 0~9: 0~9; A~Z: 10~35 -> ASCII - 55
 
     void Start()
     {
         characters = Resources.LoadAll<Sprite>("Characters");
-        word = word.ToUpper();
-        switch (word.Length)
+        input = new string(input.Where(c => !char.IsPunctuation(c)).ToArray());
+        input = input.ToUpper();
+        string[] words = input.Split(' ');
+        for (int i = 0; i < words.Length; i++)
         {
-            case 1:
-                Character(word[0], Position.CENTER);
-                break;
-            case 2:
-                Character(word[0], Position.LEFT);
-                Character(word[1], Position.RIGHT);
-                break;
-            case 3:
-                Character(word[0], Position.LEFT);
-                Character(word[1], Position.TOPRIGHT);
-                Character(word[2], Position.BASERIGHT);
-                break;
-            case 4:
-                Character(word[0], Position.TOPLEFT);
-                Character(word[1], Position.BASELEFT);
-                Character(word[2], Position.TOPRIGHT);
-                Character(word[3], Position.BASERIGHT);
-                break;
-            default:
-                break;
-
+            Vector2 center = new Vector2((i % wordNumPerLine - wordNumPerLine / 2) * wordSpacing,
+                                         (words.Length / wordNumPerLine / 2 - i / wordNumPerLine) * lineSpacing);
+            switch (words[i].Length)
+            {
+                case 1:
+                    Character(words[i][0], Position.CENTER, center);
+                    break;
+                case 2:
+                    Character(words[i][0], Position.LEFT, center);
+                    Character(words[i][1], Position.RIGHT, center);
+                    break;
+                case 3:
+                    Character(words[i][0], Position.LEFT, center);
+                    Character(words[i][1], Position.TOPRIGHT, center);
+                    Character(words[i][2], Position.BASERIGHT, center);
+                    break;
+                case 4:
+                    Character(words[i][0], Position.TOPLEFT, center);
+                    Character(words[i][1], Position.BASELEFT, center);
+                    Character(words[i][2], Position.TOPRIGHT, center);
+                    Character(words[i][3], Position.BASERIGHT, center);
+                    break;
+                default:
+                    Character(words[i][0], Position.TOPLEFT, center);
+                    Character(words[i].Length % 10, Position.BASELEFT, center);
+                    int factor = GetFactor(words[i].Length);
+                    Character(words[i][factor], Position.TOPRIGHT, center);
+                    Character(words[i][factor * 2], Position.BASERIGHT, center);
+                    break;
+            }
         }
     }
 
-    void Character(int i, Position position)
+    void Character(int i, Position position, Vector2 center)
     {
         Image charImage = Instantiate(charPrefab, canvas.transform);
         i = i > 9 ? i - 55 : i;
@@ -78,6 +93,30 @@ public class CharacterGenerator1 : MonoBehaviour
                 charImage.rectTransform.anchoredPosition = new Vector2(offsetX, -offsetY);
                 break;
         }
+        charImage.rectTransform.anchoredPosition += center;
+    }
+
+    int GetFactor(int num)
+    {
+        num = (int)Mathf.Ceil((float)num / 2);
+        int res = 2;
+        for (int i = 2; i < num; i++)
+        {
+            bool isPrimeNum = true;
+            for (int j = 2; j < i - 1; j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrimeNum = false;
+                    break;
+                }
+            }
+            if (isPrimeNum)
+            {
+                res = Mathf.Max(res, i);
+            }
+        }
+        return res;
     }
 }
 
