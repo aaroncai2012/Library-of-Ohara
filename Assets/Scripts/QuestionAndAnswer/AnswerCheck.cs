397c0d0fef0640d7b8fe4c9e41ed9ada32f0c47e
zachzhou31 <105928767+zachzhou31@users.noreply.github.com>
2023-04-18 20:39:43 -0400

    Update Wrong/Right Answer Tip

--- Assets/Scripts/QuestionAndAnswer/AnswerCheck.cs
+++ Assets/Scripts/QuestionAndAnswer/AnswerCheck.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 public class AnswerCheck : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject FireLeft,FireRight;
+    private Quaternion startRotation;
     void Start()
     {
-        
+        startRotation = transform.rotation;
     }
 
     // Update is called once per frame
@@ -22,17 +24,18 @@ public class AnswerCheck : MonoBehaviour
         if(RandomQuestion.Instance.GameEnd == 1)
         {
             if (this.GetComponent<CharacterGeneratorSingle>().input == RandomQuestion.Instance.Question.ToUpper())
-            { GameObject.Find("RespoundText").GetComponent<Text>().text = "You Did!"; }
+            { FireRight.SetActive(true); }
             else
             {
-                GameObject.Find("RespoundText").GetComponent<Text>().text = "Guess Again";
+                StartCoroutine(Swing());
+                Invoke("StopSwing", 2f);
             }
         }
         else
         {
             if (this.GetComponent<CharacterGeneratorSingle>().input == RandomQuestion.Instance.Question.ToUpper())
             {
-                    GameObject.Find("RespoundText").GetComponent<Text>().text = "Next One";
+                FireLeft.SetActive(true);
                 if (this.GetComponent<CharacterGeneratorSingle>().input == "FIRE")
                 {
                     RandomQuestion.Instance.GameEnd = 1;
@@ -55,9 +58,32 @@ public class AnswerCheck : MonoBehaviour
             }
             else
             {
-                GameObject.Find("RespoundText").GetComponent<Text>().text = "Guess Again";
+                StartCoroutine(Swing());
+                Invoke("StopSwing", 2f);
             }
         }
 
     }
+
+    IEnumerator Swing()
+    {
+        float t = 0f;
+        while (t < 2f)
+        {
+            float t1 = Mathf.PingPong(t, 2f) / 2f;
+            float angle = Mathf.Lerp(-20f, 20f, t1);
+
+            GameObject.Find("SkullImg").transform.rotation = Quaternion.Euler(0f, 0f, angle);
+            t += Time.deltaTime;
+            yield return null;
+        }
+        GameObject.Find("SkullImg").transform.rotation = startRotation;
+
+    }
+
+    void StopSwing()
+    {
+        StopCoroutine(Swing());
+    }
+
 }
